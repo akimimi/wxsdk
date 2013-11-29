ff99b4353f45741db88e3755f70885cb8f5eead2
toboto <toboto@gmail.com>
2013-11-29 21:55:04 +0800

    DEL: Remove config file

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//define your token
-define("TOKEN", "hithisiswangrui123");
-define("APPID", "wx4a9ff7e0fe89339a");
-define("APPSECRET", "f389e4e77f82e3a26656ec8cff68e313");
-define("ROOT", dirname(__FILE__));
-
